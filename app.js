741ed029d5b3153fd21f99b6057d99e3e1eb05a8
sataller <astaller@yandex.ru>
2020-11-18 15:55:16 +0300

    7

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const userRotes = require('./routes/user');
 const keys = require('./config/keys');
 const app = express();
 
-
-
 const url = process.env.MONGO_URL || keys.mongoURI;
 mongoose.connect(url,
     {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,})
@@ -27,9 +25,9 @@ app.use('/api/auth', authRotes);
 app.use('/api/history', historyRotes);
 app.use('/api/users', userRotes);
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'client/build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + 'client/build', 'index.html'));
+// });
 
 module.exports = app;
