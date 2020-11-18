a209c3867b9b1e889619cd0469977ccaddb0a40a
sataller <astaller@yandex.ru>
2020-11-18 20:43:56 +0300

    22

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const helmet = require('helmet');
-const uuidv4 = require('uuidv4');
 const path = require('path');
 const authRotes = require('./routes/auth');
 const historyRotes = require('./routes/history');
 const userRotes = require('./routes/user');
 const keys = require('./config/keys');
 const app = express();
-
-app.use((req, res, next) => {
-    // nonce should be base64 encoded
-    res.locals.styleNonce = Buffer.from(uuidv4()).toString('base64');
-    next();
-});
-
-app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: ["'self'"],
-            styleSrc: ["'self'", (req, res) => "nonce-$ {res.locals.styleNonce}"]
-        }
-    })
-);
 
 const url = process.env.MONGO_URL || keys.mongoURI;
 mongoose.connect(url,
@@ -46,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
     app.use('/', express.static(path.join('client', 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname + 'client', 'build',
-            'index.html', {styleNonce: res.locals.styleNonce}));
+            'index.html'));
     });
 }
 // app.use(express.static(path.join(__dirname, 'client/build')));
