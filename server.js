a644bbf70b30cd3a5a53e3384ae88b5c19864a10
sataller <astaller@yandex.ru>
2020-11-18 15:27:05 +0300

    3

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const path = require("path");
 
 const port = process.env.PORT || 5000;
 
-<<<<<<< HEAD
 pp.listen(port, () => {
-=======
 app.listen(port, () => {
->>>>>>> d26866315080d44d2af91b5a76de97bcd7591514
     console.log(`Server is running on ${port}`)
 })
