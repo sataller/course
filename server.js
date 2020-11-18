f3a157bb5bfcdefab220114664ca7a161658102c
sataller <56543871+sataller@users.noreply.github.com>
2020-11-18 14:16:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const path = require("path");
 
 const port = process.env.PORT || 5000;
 
-
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'client/build', 'index.html'));
-});
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
