257ff109b2ed2d339960cdb7fc2aa56687628a14
sataller <astaller@yandex.ru>
2020-11-18 15:07:38 +0300

    server update 2

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const keys = require('./config/keys');
 const passport = require('passport');
 const app = express();
 
-mongoose.connect(keys.mongoURI,
+
+const url = process.env.MONGO_URL || keys.mongoURI;
+mongoose.connect(url,
     {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,})
     .then(() => console.log("DB connected"))
     .catch(error => console.log(error));
@@ -22,5 +24,10 @@ app.use(express.json());
 app.use('/api/auth', authRotes);
 app.use('/api/history', historyRotes);
 app.use('/api/users', userRotes);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'client/build', 'index.html'));
+});
 
 module.exports = app;
