d8e6b4c19312acbff780f4572fd398e26abc6d27
sataller <astaller@yandex.ru>
2020-11-18 12:20:34 +0300

    server update

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const port = process.env.PORT || 5000;
 
 
-// app.use(express.static(path.join(__dirname, 'web-client/build')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + 'web-client/build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, 'web-client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'web-client/build', 'index.html'));
+});
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
