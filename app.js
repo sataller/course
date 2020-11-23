372381db589a584b888e956f2d28eb9c4e5be426
sataller <astaller@yandex.ru>
2020-11-23 07:17:54 +0300

    update CSP 13

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use('/api/users', userRotes);
 socket.socketComments(io);
 
 if (process.env.NODE_ENV === "production") {
-    app.use('/', express.static(path.join('client', 'build')));
+    app.use('./', express.static(path.join('client', 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname + 'client', 'build',
             'index.html'));
