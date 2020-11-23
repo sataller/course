d92527bd0804c78136b39546b0eac60153b53126
sataller <astaller@yandex.ru>
2020-11-23 08:45:08 +0300

    update CSP 16

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ socket.socketComments(io);
 //     });
 // }
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '/client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname + '/client/build', '/index.html'));
     });
 }
 
