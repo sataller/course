e2790971fd201e16d4f363f77780004b65e43199
sataller <astaller@yandex.ru>
2020-11-23 06:54:52 +0300

    update CSP 11

--- app.js
+++ app.js
@@ -38,5 +38,10 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
+// app.use(express.static(path.join(__dirname, 'web-client/build')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + 'web-client/build', 'index.html'));
+// });
+
 
 module.exports = http;
