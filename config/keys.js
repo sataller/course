2e5405165c70b343d996e11e82713dc0ff7ede35
sataller <astaller@yandex.ru>
2020-11-23 17:26:37 +0300

    finished

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://astaller:MnT9cUwx4Fi1sqUf@cluster0.rfly8.mongodb.net/courseP?retryWrites=true&w=majority',
+    jwt: "sha-jwt",
+    nodeMailer: {
+        name: "courseProjectMailer@gmail.com",
+        password: "!!!ProjectNode",
+    },
+    aws: {
+        accessKeyID: "AKIAXB7YFD6TM35DYCHI",
+        secretAccessKey: "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",
+        bucketName: "courseprojectbucket",
+        distributionId:"E1GHK4YUTUDE9S"
+    }
+};
